b607506d92d4f4e1f1c5e37e8827aa1ce52d440a
Ashish <120118449+AshishSingh297@users.noreply.github.com>
2024-10-01 22:27:44 +0530

    java + dsa
    java + dsa learning from basics  ft. winter arc

---
+++ condition.java
@@ -0,0 +1,40 @@
+
+
+//  todays code 1
+     
+// import java.util.*;
+// public class condition
+// {
+//     public static void main (String args[])
+//     {
+//         Scanner sc = new Scanner (System.in);
+//         int age = sc.nextInt();
+//         if (age >= 18)
+//         {
+//             System.out.println("wht's ur type");
+//         }
+//         else
+//         {
+//             System.out.println("no porn");
+//         }
+//     }
+// }
+
+
+//pratice code 2
+import java.util.Scanner;
+public class condition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x%2==0) 
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+
+}
+}
